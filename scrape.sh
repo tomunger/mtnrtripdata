7ad119cba33f652ce1c648b422be6b6ae62878d7
Thomas Unger <tk110@tumtum.com>
2025-03-20 12:57:18 -0700

    scrape.sh runs under cron

--- scrape.sh
+++ scrape.sh
@@ -1,3 +1,5 @@
-python src/main.py -C local-config.txt scrape
-python src/main.py -C local-config.txt scrape --profile https://www.mountaineers.org/members/will-wade
-python src/main.py -C local-config.txt scrape --profile https://www.mountaineers.org/members/colin-farrell
+cd ~/dev/src/mtntripdata
+source .venv/bin/activate
+python src/main.py -C local-config.txt scrape > local-log/"output_$(date +'%Y%m%d_%H%M%S')_unger.log"
+python src/main.py -C local-config.txt scrape --profile https://www.mountaineers.org/members/will-wade > local-log/"output_$(date +'%Y%m%d_%H%M%S')_wade.log"
+python src/main.py -C local-config.txt scrape --profile https://www.mountaineers.org/members/colin-farrell > local-log/"output_$(date +'%Y%m%d_%H%M%S')_farrell.log"
