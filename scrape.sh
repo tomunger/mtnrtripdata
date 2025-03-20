e04dfeae921ac6809406753f735ee18d9491d979
Thomas Unger <tk110@tumtum.com>
2025-03-20 06:56:49 -0700

    Add daily scrape shell script

---
+++ scrape.sh
@@ -0,0 +1,3 @@
+python src/main.py -C local-config.txt scrape
+python src/main.py -C local-config.txt scrape --profile https://www.mountaineers.org/members/will-wade
+python src/main.py -C local-config.txt scrape --profile https://www.mountaineers.org/members/colin-farrell
