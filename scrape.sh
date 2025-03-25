62e38a8515d1add8960624a494c2a8dcc7621e09
Thomas Unger <tk110@tumtum.com>
2025-03-25 13:26:06 -0700

    Update scrape.sh

--- scrape.sh
+++ scrape.sh
@@ -1,5 +1,5 @@
-cd ~/dev/src/mtntripdata
-source .venv/bin/activate
-python src/main.py -C local-config.txt scrape > local-log/"output_$(date +'%Y%m%d_%H%M%S')_unger.log"
-python src/main.py -C local-config.txt scrape --profile https://www.mountaineers.org/members/will-wade > local-log/"output_$(date +'%Y%m%d_%H%M%S')_wade.log"
-python src/main.py -C local-config.txt scrape --profile https://www.mountaineers.org/members/colin-farrell > local-log/"output_$(date +'%Y%m%d_%H%M%S')_farrell.log"
+cd /Users/unger/dev/src/mtnrtripdata/
+#source .venv/bin/activate
+uv run src/main.py -C local-config.txt scrape > local-log/"output_$(date +'%Y%m%d_%H%M%S')_unger.log"
+uv run src/main.py -C local-config.txt scrape --profile https://www.mountaineers.org/members/will-wade > local-log/"output_$(date +'%Y%m%d_%H%M%S')_wade.log"
+uv run src/main.py -C local-config.txt scrape --profile https://www.mountaineers.org/members/colin-farrell > local-log/"output_$(date +'%Y%m%d_%H%M%S')_farrell.log"
