536f308610519613a1fa6666276b2bc3110f9869
Tom Unger <tk110@tumtum.com>
2025-11-02 14:40:45 -0800

    Remove 2 files

--- scrape.sh
+++
@@ -1,4 +0,0 @@
-cd /Users/unger/dev/src/mtnrtripdata/
-uv run src/main.py scrape > local-log/"output_$(date +'%Y%m%d_%H%M%S')_unger.log"
-uv run src/main.py scrape --profile https://www.mountaineers.org/members/will-wade > local-log/"output_$(date +'%Y%m%d_%H%M%S')_wade.log"
-uv run src/main.py scrape --profile https://www.mountaineers.org/members/colin-farrell > local-log/"output_$(date +'%Y%m%d_%H%M%S')_farrell.log"
